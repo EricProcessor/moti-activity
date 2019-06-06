9e6729668f7ca57146c3ee84ae12a62e8e7d50c2
Xin0616 <1014692922@qq.com>
2019-06-06 18:10:57 +0800

    config

--- common/config.js
+++ common/config.js
@@ -3,5 +3,5 @@ config.host = process.env.NODE_ENV === 'development'
 	? 
 		"" // 开发环境
 	: 
-		'http://mall.api.motivape.cn'	// 生产环境
+		'http://test.hd.motivape.cn/api'	// 生产环境
 export default config
