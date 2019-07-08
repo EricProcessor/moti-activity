770e99c2e97d32dd03b3b368b441f800be3b580f
eric <17600100962@163.com>
2019-07-08 12:17:48 +0800

    测试

--- common/config.js
+++
@@ -1,7 +0,0 @@
-const config = {}
-config.host = process.env.NODE_ENV === 'development' 
-	? 
-		"http://mall.gray.motivape.cn/api" // 开发环境
-	: 
-		'http://motiactivity.motimall.cn/api'	// 生产环境
-export default config
