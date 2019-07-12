0757f1c36392b2f2593fca00c22d6d2bd52108b6
kuangyilong <849636007@qq.com>
2019-07-12 16:22:58 +0800

    接口地址

--- common/config.js
+++ common/config.js
@@ -3,11 +3,11 @@ config.host = process.env.NODE_ENV === 'development'
 	? 
 		"http://gghd.gray.motivape.cn/api" // 开发环境 注册
 	: 
-		'http://gghd.gray.motivape.cn/api'	// 生产环境
+		'http://zuul.prod.motivape.cn'	// 生产环境
 		
 config.hostOrder  = process.env.NODE_ENV === 'development' 
 	? 
 		"http://test.motiactivity.motimall.cn/api" // 开发环境 下单
 	: 
-		'http://test.motiactivity.motimall.cn/api'	// 生产环境		
+		'//motiactivity.motimall.cn/api'	// 生产环境		
 export default config
