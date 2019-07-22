a03167f55aeae657f40ec90dd2e211e6ff97bf7e
kuangyilong <849636007@qq.com>
2019-07-22 16:10:01 +0800

    修改

--- common/config.js
+++ common/config.js
@@ -3,16 +3,17 @@ config.host = process.env.NODE_ENV === 'development'
 	? 
 		"http://192.168.0.94:8085/api" // 开发环境 注册
 	: 
-		'http://motiactivity.motimall.cn/api'	// 生产环境
+		'http://motimall.infor8.com/api'	// 生产环境
 		
 config.hostOrder  = process.env.NODE_ENV === 'development' 
 	? 
 		"http://192.168.0.94:8085/api" // 开发环境 下单
 	: 
-		'http://motiactivity.motimall.cn/api'	// 生产环境		
+		'http://motimall.infor8.com/api'	// 生产环境		
 export default config
 //"http://192.168.1.198:8085/api"  中转 
 //http://test.motiactivity.motimall.cn/api 测试环境
 //192.168.10.48:8096 北京本地服务注册
 //192.168.10.48:9191 北京本地服务下单 
 //http://motiactivity.motimall.cn/api 正式
+//http://motimall.infor8.com/api
