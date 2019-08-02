cf364ffa5283fa62de0e5dbf16172c89ad751b3c
yaoziyun <sherfor.yao@leiyankeji.com>
2019-08-02 11:06:22 +0800

    修改api地址

--- common/js/common/config.js
+++ common/js/common/config.js
@@ -3,7 +3,7 @@ const host = window.location.host
 let api = ""
 switch (host) {
 	case "testszhd.motivape.cn":
-		api = "http://test.zuul.motivape.cn/activity"
+		api = "http://zuul.gray.motivape.cn/activity"
 		break;
 	case "szhdgray.motivape.cn":
 		api = "http://zuul.gray.motivape.cn/activity"
@@ -19,7 +19,7 @@ switch (host) {
 		api = "//192.168.10.159:8197/activity"
 		break
 }
-config.host = process.env.NODE_ENV === 'development' ? api : api         // 生产环境/线上环境
+config.host = process.env.NODE_ENV === 'development' ? api : api // 生产环境/线上环境
 
 export default config
 //"//192.168.10.129:8197/activity"
