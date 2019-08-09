4f410d6a302e5e2a67eefb3e520bf753a026fdf4
kuangyilong <849636007@qq.com>
2019-08-09 17:53:06 +0800

    提交

---
+++ pages/index/goodsMojo24.js
@@ -0,0 +1,31 @@
+export default {
+	goods: {
+		imgUrl: "",
+		price: 19.00,
+		sourcePrice: 0,
+		title: "MOJO 一次性电子烟",
+		desc:"单只装",
+		taste: [],
+	},
+	backgrounds: [
+		"../../static/images/mojo/flbs.jpg",
+		"../../static/images/mojo/jdyc.jpg",
+		"../../static/images/mojo/qsbh.jpg"
+	],
+	spec: [
+		
+		{
+			text: "凤梨冰沙",
+			sku: "112492576930"
+		},
+		{
+			text: "经典烟草",
+			sku: "112492582466"
+		},
+		
+		{
+			text: "清爽薄荷",
+			sku: "112492576122"
+		},
+	],
+}
