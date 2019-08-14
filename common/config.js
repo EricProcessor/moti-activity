c33ff157f9f8c46154b2f021260e31ea03fd8ff9
eric <17600100962@163.com>
2019-08-14 10:44:55 +0800

    新建分支

--- common/config.js
+++
@@ -1,7 +0,0 @@
-const config = {}
-config.host = process.env.NODE_ENV === 'development' 
-	? 
-		"http://test.zuul.motivape.cn" // 开发环境
-	: 
-		'http://zuul.prod.motivape.cn'	// 生产环境
-export default config
