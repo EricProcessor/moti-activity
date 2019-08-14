ae757febd85cd5fc8cad8e6a365b3ea9b448f028
eric <17600100962@163.com>
2019-08-14 14:45:21 +0800

    添加jssdk

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App'
+import jweixin from 'jweixin-module'
 
+const jweixin = require('jweixin-module')
 Vue.config.productionTip = false
 
 App.mpType = 'app'
