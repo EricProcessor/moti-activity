8f31a81570d3fc3058be525c6ef9c519af25a3aa
eric <17600100962@163.com>
2019-08-15 11:25:37 +0800

    封装请求方法

--- common/request.js
+++ common/request.js
@@ -1,6 +1,6 @@
 import config from './config.js'
 
-export function post(url, data) {
+async function post(url, data) {
 	return new Promise(function(resolve, reject) {
 		uni.request({
 			url: url,
@@ -14,5 +14,27 @@ export function post(url, data) {
 			}
 		})
 
+	})
+}
+
+async function get(url, data) {
+	return new Promise(function(resolve, reject) {
+		uni.request({
+			url: url,
+			method: 'GET',
+			data,
+			success: function(data) {
+				resolve(data)
+			},
+			fail: function(err) {
+				// 回调失败时
+				console.log(err)
+				if (typeof reject == 'function') {
+					reject(data);
+				} else {
+					console.log(data);
+				}
+			}
+		})
 	})
 }
