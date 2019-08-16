b6a71731b76ab99334b9aebc531c7538dd6b10fb
kuangyilong <849636007@qq.com>
2019-08-16 11:23:46 +0800

    修改配送信息

--- pages/index/OrderDetail.vue
+++ pages/index/OrderDetail.vue
@@ -83,7 +83,7 @@
 					</view>
 					<view class="order-receive-item">
 						<text class="ori-title">配送信息：</text>
-						<text class="ori-content">{{initData.result.deliveryInfo}}</text>
+						<text class="ori-content">{{initData.totalPrice ? initData.result.deliveryInfo : '有货，邮费12元'}}</text>
 					</view>
 					<view class="order-receive-item">
 						<text class="ori-title">订单备注：</text>
