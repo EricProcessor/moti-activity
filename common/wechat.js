3c78977d69f2207743ebf53cac894e8966528327
eric <17600100962@163.com>
2019-08-16 12:00:10 +0800

    xiugai

--- common/wechat.js
+++ common/wechat.js
@@ -17,70 +17,94 @@ export default {
 			return false;
 		}
 	},
-        //初始化sdk配置  
-    initJssdk:function(callback ,url){  
+	//初始化sdk配置  
+	initJssdk: function(callback, url) {
 		//服务端进行签名 ，可使用uni.request替换。 签名算法请看文档  
-		console.log('initJssdk', url);
-        request({
-			url: '/open/pub/wechat/jsapi/jssdk',
-			methods: 'POST',
-			contentType: 'application/json',
-			data: {
-				url: 'https://hnhd.motivape.cn/bluehd'
+		uni.request({
+			url: `http://jimi.sunadver.com/jimitd/jsSdk/getJsSdk?url=${encodeURI('https://hnhd.motivape.cn/bluehd')}`,
+			method: 'GET',
+			success(res) {
+				if (res) {
+					jweixin.config({
+						debug: false,
+						appId: 'wx683541f23442efd4',
+						timestamp: res.timestamp,
+						nonceStr: res.nonceStr,
+						signature: res.signature,
+						jsApiList: [
+							'checkJsApi',
+							'onMenuShareTimeline',
+							'onMenuShareAppMessage'
+						]
+					});
+					//配置完成后，再执行分享等功能
+					if(callback){  
+						callback(shareSgin);  
+					}  
+				}
+
 			}
-		}).then((res) => {
-			if(res.data.code != 0){
-				
-			}
-			if(res.data){
-				console.log("分享签名"+JSON.stringify(res))
-				let shareSgin = JSON.parse(res.data.result)
-				console.log("个事转化"+JSON.stringify(shareSgin))
-			    jweixin.config({  
-			         debug: false,  
-			         appId: shareSgin.appId,  
-			         timestamp:shareSgin.timestamp,  
-			         nonceStr: shareSgin.nonceStr,  
-			         signature:shareSgin.signature,  
-			         jsApiList: [  
-			             'checkJsApi',  
-			             'onMenuShareTimeline',  
-			             'onMenuShareAppMessage'  
-			         ]  
-			    });  
-			    //配置完成后，再执行分享等功能  
-			    if(callback){  
-			        callback(shareSgin);  
-			    }  
-			}  
 		})
-    },  
-        //在需要自定义分享的页面中调用  
-    share:function(data ,url){  
-        url =url ? url :window.location.href;  
-        if(!this.isWechat()){  
-            return ;  
-        }  
+		console.log('initJssdk', url);
+		//       request({
+		// 	url: '/open/pub/wechat/jsapi/jssdk',
+		// 	methods: 'POST',
+		// 	contentType: 'application/json',
+		// 	data: {
+		// 		url: encodeURI('https://hnhd.motivape.cn/bluehd') 
+		// 	}
+		// }).then((res) => {
+		// 	if(res.data.code != 0){
+		// 		
+		// 	}
+		// 	if(res.data){
+		// 		console.log("分享签名"+JSON.stringify(res))
+		// 		let shareSgin = JSON.parse(res.data.result)
+		// 		console.log("个事转化"+JSON.stringify(shareSgin))
+		// 	    jweixin.config({  
+		// 	         debug: false,  
+		// 	         appId: 'wx683541f23442efd4',  
+		// 	         timestamp:shareSgin.timestamp,  
+		// 	         nonceStr: shareSgin.nonceStr,  
+		// 	         signature:shareSgin.signature,  
+		// 	         jsApiList: [  
+		// 	             'checkJsApi',  
+		// 	             'onMenuShareTimeline',  
+		// 	             'onMenuShareAppMessage'  
+		// 	         ]  
+		// 	    });  
+		// 	    //配置完成后，再执行分享等功能  
+		// 	    if(callback){  
+		// 	        callback(shareSgin);  
+		// 	    }  
+		// 	}  
+		// })
+	},
+	//在需要自定义分享的页面中调用  
+	share: function(data, url) {
+		url = url ? url : window.location.href;
+		if (!this.isWechat()) {
+			return;
+		}
 		//每次都需要重新初始化配置，才可以进行分享  
-        this.initJssdk(function(signData){ 
-            jweixin.ready(function(){    
-                var shareData = {  
-                     title: data&&data.title ? data.title: signData.site_name,  
-                     desc: data&&data.desc ? data.desc: signData.site_description,  
-                     link: url,  
-                     imgUrl: data&&data.img ?data.img :signData.site_logo,  
-                     success: function (res) {  
-                                                 //用户点击分享后的回调，这里可以进行统计，例如分享送金币之类的  
-                        request.post('/api/member/share');  
-                     },  
-                     cancel: function (res) {  
-                     }  
-                 };  
-                 //分享给朋友接口  
-                 jweixin.onMenuShareAppMessage(shareData);  
-                 //分享到朋友圈接口  
-                 jweixin.onMenuShareTimeline(shareData);  
-            });  
-        } ,url);  
-    } 
+		this.initJssdk(function(signData) {
+			jweixin.ready(function() {
+				var shareData = {
+					title: data && data.title ? data.title : signData.site_name,
+					desc: data && data.desc ? data.desc : signData.site_description,
+					link: url,
+					imgUrl: data && data.img ? data.img : signData.site_logo,
+					success: function(res) {
+						//用户点击分享后的回调，这里可以进行统计，例如分享送金币之类的  
+						request.post('/api/member/share');
+					},
+					cancel: function(res) {}
+				};
+				//分享给朋友接口  
+				jweixin.onMenuShareAppMessage(shareData);
+				//分享到朋友圈接口  
+				jweixin.onMenuShareTimeline(shareData);
+			});
+		}, url);
+	}
 }
