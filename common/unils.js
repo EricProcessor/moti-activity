7b35f593d1899106e5da6d5221a4ae5829e9aae8
kuangyilong <849636007@qq.com>
2019-08-27 10:27:52 +0800

    提交

--- common/unils.js
+++ common/unils.js
@@ -9,9 +9,9 @@ export const saveType = 'poke'
 
 export const orderSource = "1000000" //我的订单列表
 
-export const daojiaUrl = (window.host === 'poke.motivape.cn') ? 'https://daojia.motivape.cn' : 'https://daojia.gray.motivape.cn'
+export const daojiaUrl = (window.location.host === 'poke.motivape.cn') ? 'https://daojia.motivape.cn' : 'https://daojia.gray.motivape.cn'
 
-export const isDevEnv = (window.host === 'poke.motivape.cn') ? false : true
+export const isDevEnv = (window.location.host === 'poke.motivape.cn') ? false : true
 
 //export const daojiaUrl = 'https://daojia.gray.motivape.cn'
 
