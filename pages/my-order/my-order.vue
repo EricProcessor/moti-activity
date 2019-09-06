9006b2f3e722369b08d6166db1fc9403b2c2c379
yaoziyun <sherfor.yao@leiyankeji.com>
2019-09-06 15:05:43 +0800

    修改时间

--- pages/my-order/my-order.vue
+++ pages/my-order/my-order.vue
@@ -136,7 +136,7 @@
 						<image class="qrcode-image" :src="qrUrl"></image>
 					</view> -->
 					<view class="tip-one">扫描二维码进店</view>
-					<view class="tip-two">活动时间：9月1日-10月31日</view>
+					<view class="tip-two">活动时间：9月1日-10月30日</view>
 					<view id="ignore-btton">
 						<button class="save-btn" @click="saveImage">保存图片</button>
 					</view>
