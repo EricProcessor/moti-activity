457fda8b1450a51e64ca633cdfe443a18fb4d5d3
eric <wamk08@sina.com>
2019-09-24 20:19:53 +0800

    跳转

--- pages/index/MfwFen.vue
+++ pages/index/MfwFen.vue
@@ -1,7 +1,7 @@
 <template>
 	<view class="mfw-fen">
-		<img style="width:750upx" src="../../static/images/mfw/ad.jpg"  />
-		<img style="width:750upx" @tap="jumpTo" src="../../static/images/mfw/ad-btn.jpg"  />
+		<img style="width:750upx" @load ="imgFinish"  @tap="jumpTo" src="../../static/images/mfw/ad.jpg"  />
+		<img style="width:750upx" @load ="imgFinish" id="acher" @tap="jumpTo" src="../../static/images/mfw/ad-btn.jpg"  />
 	</view>
 </template>
 
@@ -9,14 +9,29 @@
 	export default{
 		data(){
 			return {
-				
+				count:0
 			}
 		},
 		methods:{
+			imgFinish(){
+				this.count++
+				if(this.count >= 2){
+					this.scrollToBottom()
+				}
+			},
 			jumpTo(){
 				location.href = location.origin + "?type=65&orderSource=1002&channel=mafengwo&material=001"
+			},
+			scrollToBottom(){
+				let d = document.getElementById("acher")
+				
+				uni.pageScrollTo({
+					scrollTop:d.offsetTop,
+					duration:300
+				})
 			}
 		}
+		
 	}
 </script>
 
